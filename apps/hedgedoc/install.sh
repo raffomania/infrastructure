612dbf7fef624e14c0af21eaa16ef34b34be93e7
Rafael Epplée <hello@rafa.ee>
2023-10-02 21:34:56 +0200

    Use symlinks for quadlet

--- apps/hedgedoc/install.sh
+++ apps/hedgedoc/install.sh
@@ -8,8 +8,8 @@ podman secret exists hedgedoc-session-secret || \
 podman secret exists hedgedoc-db-password || \
     pwgen -s 64 1 | podman secret create hedgedoc-db-password -
 
-cp -t ~/.config/containers/systemd/ \
-    ./*.container ./*.network ./*.volume
+ln -rsf ./*.container ./*.network ./*.volume \
+    ~/.config/containers/systemd/
 
 # Check that quadlet runs successfully
 /usr/lib/systemd/system-generators/podman-system-generator --user --dryrun 1>/dev/null
