fc84b1fce60e696cf6d213f1855751f74e7792f4
Rafael Epplée <rafael@rafa.ee>
2024-10-28 21:43:33 +0100

    Move to compose file for hedgedoc

--- apps/hedgedoc/install.sh
+++ apps/hedgedoc/install.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 set -euxo pipefail
 
-podman secret exists hedgedoc-session-secret || \
-    pwgen -s 64 1 | podman secret create hedgedoc-session-secret -
-
-podman secret exists hedgedoc-db-password || \
-    pwgen -s 64 1 | podman secret create hedgedoc-db-password -
-
-ln -rsf ./*.container ./*.network ./*.volume \
-    ~/.config/containers/systemd/
-
-check-quadlet.sh
-
-systemctl --user daemon-reload
+test -f ./session-secret || \
+    echo $(pwgen -s 64 1) > session-secret
