5a019d9a014dfe6b56cde3761dd0b90ec09b8ef3
Rafael Epplée <rafael@rafa.ee>
2024-10-28 11:18:16 +0100

    Fix postgres update script

--- apps/miniflux/update_postgres.sh
+++ apps/miniflux/update_postgres.sh
@@ -2,27 +2,27 @@
 set -euxo pipefail
 
 # Dump all data from the old database version to a single file
-podman exec miniflux_db_1 pg_dumpall -U "miniflux" > dump.sql
+podman exec miniflux-db-1 pg_dumpall -U "miniflux" > dump.sql
 
 # Stop all containers
-podman-compose stop
+podman compose stop
 
 # Backup data volume
 podman volume export miniflux_miniflux-db -o ~/backups/miniflux-db-volume-backup.tar
 
 # remove all containers and volume
-podman-compose down --volumes
+podman compose down --volumes
  
 # Start the new container and initialize a blank database instance
-podman-compose up db -d
+podman compose up db -d
 
 # Wait for db to start
 sleep 5
 
 # Import the dumped file into the database instance
-podman exec -i miniflux_db_1 sh -c 'psql -U "miniflux" -d "miniflux"' < dump.sql
+podman exec -i miniflux-db-1 sh -c 'psql -U "miniflux" -d "miniflux"' < dump.sql
 
 # re-start the rest
-podman-compose up -d
+podman compose up -d
 
 echo "check if everything works, then remove dump.sql and db-volume-backup.tar"
