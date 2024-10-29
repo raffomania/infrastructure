84256a21560933606c544383447db8e7398e9c39
Rafael Epplée <rafael@rafa.ee>
2024-10-29 15:38:24 +0100

    Add generic postgres update script

--- apps/hedgedoc/update_postgres.sh
+++ apps/hedgedoc/update_postgres.sh
@@ -1,31 +1,9 @@
 #!/usr/bin/sh
 set -euxo pipefail
 
-systemctl --user stop hedgedoc
+cd $(dirname $0)
 
-# Dump all data from the old database version to a single file
-podman exec hedgedoc-postgres pg_dumpall -U "hedgedoc" > dump.sql
+echo $(pwd)
 
-# Stop db container
-systemctl --user stop hedgedoc-postgres
-
-# Backup data volume
-podman volume export systemd-hedgedoc-db -o ~/backups/hedgedoc-db-volume-backup.tar
-
-# remove db volume
-podman volume rm systemd-hedgedoc-db
- 
-# Start the new container and initialize a blank database instance
-systemctl --user daemon-reload
-systemctl --user start hedgedoc-postgres
-
-# Wait for db to start
-sleep 5
-
-# Import the dumped file into the database instance
-podman exec -i hedgedoc-postgres sh -c 'psql -U "hedgedoc" -d "hedgedoc"' < dump.sql
-
-# re-start the rest
-systemctl --user start hedgedoc
-
-echo "check if everything works, then remove dump.sql and ~/backups/hedgedoc-db-volume-backup.tar"
+db_container=hedgedoc-db-1 db_username=hedgedoc db_volume=systemd-hedgedoc-db \
+    update-generic-postgres.sh
---
+++ bin/update-generic-postgres.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/sh
+set -euxo pipefail
+
+# Dump all data from the old database version to a single file
+podman exec "$db_container" pg_dumpall -U "$db_username" > dump.sql
+
+[[ ! -s dump.sql ]] && echo "dump.sql is empty" && exit 1
+
+# Stop all containers
+podman compose stop
+
+# Backup data volume
+volume_backup_file="$HOME/backups/$db_volume-volume-backup.tar"
+podman volume export "$db_volume" -o $volume_backup_file
+
+[[ ! -s $volume_backup_file ]] && echo "$volume_backup_file is empty" && exit 1
+
+# remove db container & volume
+podman compose down db
+podman volume rm "$db_volume"
+ 
+# Start the new container and initialize a blank database instance
+podman compose up db -d
+
+# Wait for db to start
+podman wait --condition=healthy "$db_container"
+
+# Import the dumped file into the database instance
+podman exec -i "$db_container" sh -c "psql -U $db_username" < dump.sql
+
+# re-start the rest
+podman compose up -d
+
+echo "check if everything works, then remove dump.sql and db volume backup in ~/backups"
